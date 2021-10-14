4d751a7b2a78bc0e67fa553b56653ec77e1cc69b
PavelValger <pavelwalker@mail.ru>
2021-10-14 19:28:35 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Наличие ошибки " + active);
+        System.out.println(("Номер ошибки " + status));
+        System.out.println("Требуемое действие " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 1, "reboot the system");
+        Error error2 = new Error(true, 2, "reinstall the graphics card driver");
+        error.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+    }
+}
