292487b9539a1b6fdef5333288360246b3c0c8e6
PavelValger <pavelwalker@mail.ru>
2021-11-04 20:22:38 +0500

    3.2. Зачем нужно приведение типов [#310019] + ветвление

--- src/main/java/ru/job4j/oop/MoveVehicle.java
+++ src/main/java/ru/job4j/oop/MoveVehicle.java
@@ -8,13 +8,13 @@ public class MoveVehicle {
         Vehicle express = new Train();
         Vehicle intercity = new Bus();
         Vehicle urban = new Bus();
-        Vehicle[] vehicles = new Vehicle[]{boeing, lastochka, intercity};
-        for (Vehicle transport : vehicles) {
-            transport.move();
-        }
-        Vehicle[] speed = new Vehicle[]{airbus, express, urban};
-        for (Vehicle transport : speed) {
-            transport.speed();
+        Vehicle[] vehicles = new Vehicle[]{boeing, airbus, lastochka, express, intercity, urban};
+        for (int index = 0; index < vehicles.length; index++) {
+            if (index % 2 == 0) {
+                vehicles[index].move();
+            } else {
+                vehicles[index].speed();
+            }
         }
     }
 }
