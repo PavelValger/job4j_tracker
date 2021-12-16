e474f851dbb32d3d49fb0c5036cc2506c2a08f1f
PavelValger <pavelwalker@mail.ru>
2021-12-16 13:06:37 +0500

    1. Телефонный справочник на базе ArrayList [#41598]. Разделил тесты, а также добавил тест "whenEmpty" - когда по критерию ничего не будет найдено и метод вернет пустой список.

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.search;
 
 import org.junit.Test;
+
 import java.util.ArrayList;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,11 +16,41 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk"));
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
-        ArrayList<Person> surname = phones.find("Ars");
-        assertThat(surname.get(0).getName(), is("Petr"));
-        ArrayList<Person> phone = phones.find("87");
-        assertThat(phone.get(0).getName(), is("Petr"));
-        ArrayList<Person> address = phones.find("yan");
-        assertThat(address.get(0).getName(), is("Petr"));
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Ars");
+        assertThat(persons.get(0).getName(), is("Petr"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("87");
+        assertThat(persons.get(0).getAddress(), is("Bryansk"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("yan");
+        assertThat(persons.get(0).getPhone(), is("534872"));
+    }
+
+    @Test
+    public void whenEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk"));
+        ArrayList<Person> persons = phones.find("Pavel");
+        assertThat(persons.isEmpty(), is((true)));
     }
 }
