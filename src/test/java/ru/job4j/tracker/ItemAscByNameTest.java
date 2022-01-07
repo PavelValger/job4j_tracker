53700960a07f5c3f45d8ef09820a2ca261fac901
PavelValger <pavelwalker@mail.ru>
2022-01-07 20:06:27 +0500

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemAscByNameTest {
+
+    @Test
+    public void compare() {
+        List<Item> items = Arrays.asList(
+                new Item("Qwerty"),
+                new Item("Denis"),
+                new Item("Amadey"),
+                new Item("Felix"));
+        List<Item> expected = Arrays.asList(
+                new Item("Amadey"),
+                new Item("Denis"),
+                new Item("Felix"),
+                new Item("Qwerty"));
+        items.sort(new ItemAscByName());
+        assertThat(items, is(expected));
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemDescByNameTest {
+
+    @Test
+    public void compare() {
+        List<Item> items = Arrays.asList(
+                new Item("Amadey"),
+                new Item("Qwerty"),
+                new Item("Felix"),
+                new Item("Denis"));
+        List<Item> expected = Arrays.asList(
+                new Item("Qwerty"),
+                new Item("Felix"),
+                new Item("Denis"),
+                new Item("Amadey"));
+        items.sort(new ItemDescByName());
+        assertThat(items, is(expected));
+    }
+}
