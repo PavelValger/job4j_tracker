8750867e25df43de20514a07455404be9eafeeeb
PavelValger <pavelwalker@mail.ru>
2022-01-07 20:46:09 +0500

    0. Сортировка [#10096]. Добавил перенос строки

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -27,7 +27,7 @@ public class Item {
     @Override
     public String toString() {
         return "Item{" + "id = " + id + ", name = '" + name + '\''
-                + ", created = " + created.format(FORMATTER) + '}';
+                + ", created = " + created.format(FORMATTER) + '}' + System.lineSeparator();
     }
 
     public LocalDateTime getCreated() {
