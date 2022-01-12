4ff9138e29d01aa5da042f05f6fa04fd17d56894
PavelValger <pavelwalker@mail.ru>
2022-01-12 15:57:49 +0500

    3. Компаратор для строк [#35008]. Упростил код.

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -1,32 +1,19 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
         int rsl = 0;
-        List<Character> one = new ArrayList<>();
-        List<Character> two = new ArrayList<>();
-        for (int index = 0; index < left.length(); index++) {
-            one.add(left.charAt(index));
-        }
-        for (int index = 0; index < right.length(); index++) {
-            two.add(right.charAt(index));
-        }
-        int limit = Math.min(one.size(), two.size());
+        int limit = Math.min(left.length(), right.length());
         for (int index = 0; index < limit; index++) {
-            if (one.get(index) != two.get(index)) {
-                rsl = Character.compare(one.get(index), two.get(index));
+            if (left.charAt(index) != right.charAt(index)) {
+                rsl = Integer.compare(left.charAt(index), right.charAt(index));
                 break;
             }
         }
-        if (rsl == 0) {
-            rsl = one.size() - two.size();
-        }
-        return rsl;
+        return rsl == 0 ? left.length() - right.length() : rsl;
     }
 }
