d410756cb23d7524df8cc5b9f95e93bcd97b9227
Pavel Valger <pavelwalker@mail.ru>
2023-02-21 21:06:20 +0500

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private static final String NEW_VALUE = "";
+    private final int sizeOfEmpty = 10;
+    private String surname;
+    private String name;
+
+    public Broken() {
+    }
+
+    private void echo() {
+    }
+
+    public void turnOnMedia(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void executeMethod(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
