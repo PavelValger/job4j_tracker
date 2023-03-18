0fc7e3db00ceb98e2e98d0c4999bea40898f7972
Pavel Valger <pavelwalker@mail.ru>
2023-03-18 19:42:53 +0500

    2. Парковка машин [#853] удалил лишнее

--- src/main/java/ru/job4j/ood/lsp/parking/calculator/PlacesCalculator.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ood.lsp.parking.calculator;
-
-import ru.job4j.ood.lsp.parking.model.TransportMeans;
-
-public interface PlacesCalculator {
-    boolean calculation(TransportMeans transportMeans);
-}
